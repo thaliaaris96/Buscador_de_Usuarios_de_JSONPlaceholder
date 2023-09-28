691d198a4c5f772cf2cd55c1a69fc95bceed991c
Thalia Arismendi <thaliaiba96@gmail.com>
2023-09-28 20:34:43 -0300

    actualizando js

--- js/index.js
+++ js/index.js
@@ -61,3 +61,62 @@ function mostrar(items){
 
     resultado.appendChild(newDiv);
     };
+
+
+/*
+const btnB = document.getElementById("btnBusqueda");
+const resultado = document.querySelector(".contenedorResultado"); // Usar querySelector para obtener un elemento
+const inp = document.querySelector(".inputNombre"); // Usar querySelector para obtener un elemento
+const api = "https://jsonplaceholder.typicode.com/users";
+
+async function buscarUsuario(input) {
+    try {
+        const response = await fetch(api);
+        const data = await response.json(); // Convertir la respuesta a JSON
+        mostrar(data, input);
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+btnB.addEventListener('click', function(){
+    const user = inp.value;
+    buscarUsuario(user);
+})
+
+function mostrar(items, nombreUsuario) {
+    resultado.innerHTML = "";
+    const usuarioEncontrado = items.find(item => item.username === nombreUsuario);
+
+    if (!usuarioEncontrado) {
+        resultado.innerHTML = "No se encontró un usuario con ese nombre";
+        return;
+    }
+
+    const newDiv = document.createElement("div");
+    newDiv.className = "divResultado";
+
+    const name = usuarioEncontrado.name;
+    const username = usuarioEncontrado.username;
+    const email = usuarioEncontrado.email;
+    const address = usuarioEncontrado.address.street; // Modificar aquí para obtener la dirección correcta
+
+    newDiv.innerHTML = `
+        <section id="contName">
+            <p>${name}</p>
+        </section>
+        <section id="contUsername">
+            <p>${username}</p>
+        </section>
+        <section id="contEmail">
+            <p>${email}</p>
+        </section>      
+        <section id="contAddress">
+            <p>${address}</p>
+        </section> 
+    `;
+
+    resultado.appendChild(newDiv);
+}
+
+*/
